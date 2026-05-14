5bf42ab62051a8fb2cb7af48221f244e4fa59af0
leybraks <leybrak22@gmail.com>
2026-05-14 11:00:01 -0500

    desde el servidor smtp gmail

--- core/settings.py
+++ core/settings.py
@@ -259,13 +259,13 @@ APIS_NET_PE_TOKEN = os.environ.get('APIS_NET_PE_TOKEN', '')
 
 # settings.py
 # Reemplaza las últimas líneas de email por esto:
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+EMAIL_BACKEND     = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST        = os.environ.get('EMAIL_HOST', 'smtp.zoho.com')
+EMAIL_PORT        = int(os.environ.get('EMAIL_PORT', 587))
+EMAIL_USE_TLS     = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_HOST_USER   = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
-DEFAULT_FROM_EMAIL = f'BravaPOS <{os.environ.get("EMAIL_HOST_USER", "")}>'
+DEFAULT_FROM_EMAIL  = os.environ.get('DEFAULT_FROM_EMAIL', 'Leybrak Tech <soporte@leybrak.com>')
 
 from django.templatetags.static import static
 from django.urls import reverse_lazy
